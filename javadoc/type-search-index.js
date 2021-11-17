45c527c65dbdfcfbbbc5bd366ea7ccb9c0e4a54a
tsvya <tsvyakh.vova@mail.ru>
2021-11-18 03:12:33 +0700

    add full javadoc

---
+++ javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"com.example.p1","l":"ExampleInstrumentedTest"},{"p":"com.example.p1","l":"ExampleUnitTest"},{"p":"com.example.p1","l":"Main"},{"p":"com.example.p1","l":"MainTest"}]
